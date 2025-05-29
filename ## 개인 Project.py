92e2c08502bf2084d087e3c3801a6554a0fe2092
Eugene <eugene1011@snu.ac.kr>
2025-05-29 20:24:29 +0900

    Determined the YOA Project Theme!

---
+++ ## 개인 Project.py
@@ -0,0 +1,5 @@
+## 개인 Project
+# LIN28A의 mRNA 타겟 예측을 위한 CLIP-seq + Ribo-seq 통합 분석
+
+# Purpose : LIN28A가 직접 결합하는 mRNA를 CLIP-seq으로 식별하고, 이들 중 translation에 어떤 영향을 주는지를 Ribo-seq으로 확인 
+# CLIP-seq과 Ribo-seq 데이터를 통합 분석하여 LIN28A의 직접적인 mRNA 타겟을 예측하고, 이들 타겟의 번역 효율(translation efficiency, TE)이 어떻게 변화하는지 확인!
